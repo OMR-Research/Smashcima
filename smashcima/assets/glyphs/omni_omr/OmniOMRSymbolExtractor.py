a6b0adfc3981496c0a857a7501965647e34a0037
Jirka Mayer <jirkamayer97@gmail.com>
2025-03-27 13:55:29 +0100

    OmniOMR symbol binarization

--- smashcima/assets/glyphs/omni_omr/OmniOMRSymbolExtractor.py
+++ smashcima/assets/glyphs/omni_omr/OmniOMRSymbolExtractor.py
@@ -36,23 +36,78 @@ class OmniOMRSymbolExtractor(MungSymbolExtractor):
         )
     
     def sprite_bitmap_from_mung_node(self, node: Node) -> np.ndarray:
-        # crop out the image behind the node
-        image_bgr = self.page_image[
-            node.top:node.bottom,
-            node.left:node.right,
-            :
-        ]
+        # We tested a few variants of extracting sprites from the dataset:
+        # return _mask_defines_alpha(self.page_image, node)
+        # return _lightness_defines_alpha_under_mask(self.page_image, node)
+        return _binarized_adjusted_lightness(self.page_image, node)
 
-        # use mask to determine the alpha channel
-        alpha = node.mask * 255
-        bitmap = np.concatenate([image_bgr, alpha[:,:,np.newaxis]], axis=2)
 
-        # get lightness of the original image
-        # image_hls = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2HLS)
-        # lightness = image_hls[:,:,1]
+def _mask_defines_alpha(page_image: np.ndarray, node: Node) -> np.ndarray:
+    """Uses the mask to set the alpha channel, making the symbol look
+    cropped out of the original image by the contours of the mask."""
+    # crop out the image behind the node
+    image_bgr = page_image[node.top:node.bottom, node.left:node.right, :]
+    
+    # define alpha channel by the mask
+    image_alpha = node.mask * 255
 
-        # adjust alpha channel by lightness
-        # (dark areas are less transparent)
-        # bitmap[:,:,3] = (255 - lightness) * node.mask
+    # combine BGR with the alpha channel
+    return np.concatenate([image_bgr, image_alpha[:,:,np.newaxis]], axis=2)
 
-        return bitmap
+
+def _lightness_defines_alpha_under_mask(
+    page_image: np.ndarray,
+    node: Node
+) -> np.ndarray:
+    """Uses the original image lightness as alpha value for the symbol,
+    but only under the mask. Elsewhere sets alpha to zero. Makes symbols
+    very translucent."""
+    # crop out the image behind the node
+    image_bgr = page_image[node.top:node.bottom, node.left:node.right, :]
+
+    # use mask to determine the alpha channel
+    alpha = node.mask * 255
+    bitmap = np.concatenate([image_bgr, alpha[:,:,np.newaxis]], axis=2)
+
+    # get lightness of the original image
+    image_hls = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2HLS)
+    lightness = image_hls[:,:,1]
+
+    # adjust alpha channel by lightness
+    # (dark areas are less transparent)
+    bitmap[:,:,3] = (255 - lightness) * node.mask
+
+    return bitmap
+
+
+def _binarized_adjusted_lightness(
+    page_image: np.ndarray,
+    node: Node
+) -> np.ndarray:
+    # crop out the image behind the node
+    image_bgr = page_image[node.top:node.bottom, node.left:node.right, :]
+
+    # get lightness of the original image
+    image_hls = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2HLS)
+    lightness = image_hls[:,:,1]
+
+    # apply Otsu binarization (blur creates two distinc modalities - ink&paper
+    # and Otsu finds the midpoint between the two to use as the threshold)
+    # also, use bilateral filter blur to preserve edges instead of gaussian
+    blurred_lightness = cv2.bilateralFilter(lightness, 5, 25, 25)
+    _, binarized = cv2.threshold(
+        blurred_lightness, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU
+    )
+
+    # set everything outside the mask to white
+    binarized[node.mask == 0] = 255
+
+    # convert the black-on-white image to black-on-alpha
+    bitmap = np.zeros(shape=(node.height, node.width, 4), dtype=np.uint8)
+    bitmap[:,:,3] = 255 - binarized
+    return bitmap
+
+    # dummy preview (grayscale)
+    # return np.stack([
+    #     binarized, binarized, binarized, np.ones_like(lightness) * 255
+    # ], axis=2)
